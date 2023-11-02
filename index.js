5d6dccab96e03e837cbd7b74b9452309f55b8c5e
Zach Willis <zachmw27@gmail.com>
2023-11-01 23:10:01 -0400

    complete

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(freeRide){
   // Write your code here!
+  if (freeRide <= 400) {
+    return 'This one is on me!'
+  } else if (freeRide <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (freeRide > 2500) {
+    return 'No can do.'
+  } else if (freeRide > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destinationCity){
   // Write your code here!
+  const passengerCity = (destinationCity !== "NYC") ? "No go." : "Ok, sounds good.";
+    return passengerCity;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    case "anything else":
+      return "Bye.";
+    default:
+      return "Bye.";
+  }
 }
